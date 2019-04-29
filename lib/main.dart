dbcd61c842ec1c6c84659538f49f62e7d65eee38
raymond van der velden <raymond@iMac-van-chappie.local>
2019-04-29 15:48:45 +0200

    fixed not having to settext
    listerner active on main body.

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,6 @@ import 'package:goedale_client/pages/cart_page.dart';
 import 'package:goedale_client/pages/history_page.dart';
 import 'package:goedale_client/pages/admin_page.dart';
 
-import 'package:goedale_client/functions/globals.dart'; //weg?
-
-
 
 void main() => runApp(MyApp(BeerTable()));
 
@@ -52,7 +49,7 @@ class HomeWidget extends StatefulWidget {
 PageController pageController;
 
 class _HomeWidgetState extends State<HomeWidget> {
-  var appBarTitleText = new Text("Goedale");
+  var appBarTitleText = "Goedale";
   int _page = 0;
 
   String tableNumber;
@@ -73,110 +70,114 @@ class _HomeWidgetState extends State<HomeWidget> {
 //  }
 
   setAppBarTitle() {
-    tableNumber = ScopedModel.of<BeerTable>(context).tableNumber;
     if (_page == 0) {
-      appBarTitleText = Text("Aanbod - Goedale - Tafel: " + tableNumber);
+      appBarTitleText = "Aanbod - Goedale - Tafel: " ;
     } else if (_page == 1) {
-      appBarTitleText = Text("Winkelwagen - Goedale - Tafel: " + tableNumber);
+      appBarTitleText = "Winkelwagen - Goedale - Tafel: ";
     } else if (_page == 2) {
-      appBarTitleText = Text("Bestellingen - Goedale - Tafel: " + tableNumber);
+      appBarTitleText = "Bestellingen - Goedale - Tafel: ";
     }
   }
 
   Widget build(BuildContext context) {
-        return Scaffold(
-            drawer: Drawer(
-              child: ListView(
-                // Important: Remove any padding from the ListView.
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  DrawerHeader(
-                    child: Text('Goedale - Bokaal'),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
+        return ScopedModelDescendant<BeerTable>(
+          builder: (context, child, beerTableModel){
+            return  Scaffold(
+                drawer: Drawer(
+                  child: ListView(
+                    // Important: Remove any padding from the ListView.
+                    padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      DrawerHeader(
+                        child: Text('Goedale - Bokaal'),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('Beheer'),
+                        onTap: () {
+                          _displayDialog(context);
+                        },
+                      ),
+                      ListTile(
+                        title: Text('Instellingen'),
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                appBar: AppBar(
+                    title: Text(
+                    appBarTitleText + beerTableModel.tableNumber)
+                ),
+                body: PageView(
+                  children: <Widget>[
+                    Container(
+                      child: StockPage(),
                     ),
-                  ),
-                  ListTile(
-                    title: Text('Beheer'),
-                    onTap: () {
-                      _displayDialog(context);
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Instellingen'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
-            appBar: AppBar(
-                title:
-                appBarTitleText //Text((_page == 0) ?  "Aanbod" : "Winkelwagen" + ' - Goedale - Tafel: ' + tableNumber),
-            ),
-            body: PageView(
-              children: <Widget>[
-                Container(
-                  child: StockPage(),
+                    Container(
+                      child: CartPage(),
+                    ),
+                    Container(
+                      child: HistoryPage(),
+                    ),
+                  ],
+                  controller: pageController,
+                  onPageChanged: onPageChanged,
                 ),
-                Container(
-                  child: CartPage(),
-                ),
-                Container(
-                  child: HistoryPage(),
-                ),
-              ],
-              controller: pageController,
-              onPageChanged: onPageChanged,
-            ),
-            bottomNavigationBar: SizedBox(
-              height: 70,
-              child: CupertinoTabBar(
-                items: <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.home,
-                        color: (_page == 0) ? Colors.white : Colors.grey,
-                      )),
-                  BottomNavigationBarItem(
-                    icon:  Stack(
-                      children: <Widget>[
-                        Icon(Icons.shopping_cart, color: (_page == 1) ? Colors.white : Colors.grey),
-                        Positioned(
-                          right: 0,
-                          child:  Container(
-                            padding: EdgeInsets.all(1),
-                            decoration:  BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                            constraints: BoxConstraints(
-                              minWidth: 12,
-                              minHeight: 12,
-                            ),
-                            child:  Text(
-                              '5',//todo somehow manage state better to get cart items here.
-                              style:  TextStyle(
-                                color: Colors.white,
-                                fontSize: 8,
+                bottomNavigationBar: SizedBox(
+                  height: 70,
+                  child: CupertinoTabBar(
+                    items: <BottomNavigationBarItem>[
+                      BottomNavigationBarItem(
+                          icon: Icon(
+                            Icons.home,
+                            color: (_page == 0) ? Colors.white : Colors.grey,
+                          )),
+                      BottomNavigationBarItem(
+                        icon:  Stack(
+                          children: <Widget>[
+                            Icon(Icons.shopping_cart, color: (_page == 1) ? Colors.white : Colors.grey),
+                            Positioned(
+                              right: 0,
+                              child:  Container(
+                                padding: EdgeInsets.all(1),
+                                decoration:  BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(6),
+                                ),
+                                constraints: BoxConstraints(
+                                  minWidth: 12,
+                                  minHeight: 12,
+                                ),
+                                child:  Text(
+                                  '5',//todo somehow manage state better to get cart items here.
+                                  style:  TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 8,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),),
-                  BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.history,
-                        color: (_page == 2) ? Colors.white : Colors.grey,
-                      )),
-                ],
-                onTap: navigationTapped,
-                currentIndex: _page,
-              ),
-            ));
+                            )
+                          ],
+                        ),),
+                      BottomNavigationBarItem(
+                          icon: Icon(
+                            Icons.history,
+                            color: (_page == 2) ? Colors.white : Colors.grey,
+                          )),
+                    ],
+                    onTap: navigationTapped,
+                    currentIndex: _page,
+                  ),
+                ));
+          },
+
+        );
 
   }
 
@@ -225,7 +226,8 @@ class _HomeWidgetState extends State<HomeWidget> {
                   if (_textFieldController.text == "1234") {
                     _textFieldController.text = "";
                     Navigator.of(context).pop();
-                    openAdminAndWait(context)
+                   // openAdminAndWait(context);
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => AdminPage()));
                  //   _getAdminSettings(); //remove?
                   } else{
                     print("wrong pw");
@@ -236,11 +238,7 @@ class _HomeWidgetState extends State<HomeWidget> {
           );
         });
   }
-openAdminAndWait(BuildContext context) async {
-  final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => AdminPage()));
-// todo fix this wait, this wait just updates the title after the context has closed. because we can't somehow change it otherwise
-  setAppBarTitle();
-}
+
 
 }
 
