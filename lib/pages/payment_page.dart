2587a63d283e4f88a58bd59100541397a9289791
raymond van der velden <raymond@iMac-van-chappie.local>
2019-05-06 14:02:15 +0200

    Added QR code + payment with stripe.
    also hidden button.

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:goedale_client/scoped_model/global_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class PaymentPage extends StatefulWidget {
   @override
@@ -82,17 +83,54 @@ class _PaymentPageState extends State<PaymentPage> {
                 body: Container(
                     child: Center(
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: <Widget>[
-                          Text("Bestelling: " + globalModel.currentOrderId),
-                          Text("Bedrag: " + globalModel.totalAmount.toString()),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                                "Scan de QR code met je telefoon of ga naar:",
+                                style: Theme.of(context).textTheme.title),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("https://raymond.notive.app/pay/" + globalModel.currentOrderId +"/"+ globalModel.tableNumber,
+                                style: TextStyle(fontSize: 25.0, color: Colors.red, decoration: TextDecoration.underline, fontWeight: FontWeight.bold)
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                                "om je bestelling te betalen",
+                                style: Theme.of(context).textTheme.title),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              color: Colors.white,
+                              child: QrImage(
+                                  data: "https://raymond.notive.app/pay/" + globalModel.currentOrderId +"/"+ globalModel.tableNumber,
+                                  size: 500
+                              ),
+                            ),
+                          ),
+                        /*  Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Bestelling: " + globalModel.currentOrderId,style: Theme.of(context).textTheme.title),
+                          ),*/
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Te betalen bedrag: €" + globalModel.totalAmount.toString(),style: Theme.of(context).textTheme.title),
+                          ),
+
                           SizedBox(
-                            width: 125,
+                            height: 5,
+                            width: 5,
                             child: RaisedButton(
+                              color: Colors.grey[850],
                               child: Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: <Widget>[
-                                  Text('Betaald '),
-                                  Icon(Icons.check),
+
                                 ],
                               ),
                               onPressed: () async {
