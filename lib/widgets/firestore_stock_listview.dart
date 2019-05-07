9f940ca33c576b56045703d4d330c7f7bfbfde44
raymond van der velden <raymond@iMac-van-chappie.local>
2019-05-07 10:25:03 +0200

    Added search to beer page.
    Searching on desc, name, style, brewery.

--- lib/widgets/firestore_stock_listview.dart
+++ lib/widgets/firestore_stock_listview.dart
@@ -9,90 +9,204 @@ import 'package:goedale_client/functions/utils.dart';
 import 'package:goedale_client/pages/beerdetail_page.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-class FirestoreStockListview extends StatelessWidget {
+class FirestoreStockListview extends StatefulWidget {
   final List<DocumentSnapshot> beersInStock;
 
   FirestoreStockListview({this.beersInStock});
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: beersInStock.length,
-      itemBuilder: (BuildContext context, int index) {
-        var _listing = BeerListing(
-          beer: Beer(
-            id: beersInStock[index].data['id'].toString(),
-            name: beersInStock[index].data['name'].toString(),
-            brewery: beersInStock[index].data['brewery'].toString(),
-            label: BeerLabel(
-                iconUrl: beersInStock[index].data['label']['iconUrl'].toString()),
-            style: BeerStyle(
-                id: null, name: beersInStock[index].data['style'].toString()),
-            abv: beersInStock[index].data['abv'],
-            rating: beersInStock[index].data['rating'],
-          ),
-          price: beersInStock[index].data['price'],
-          stockAmount: beersInStock[index].data['amount'],
-        );
+  _FirestoreStockListviewState createState() => _FirestoreStockListviewState();
+}
 
-        if (_listing.stockAmount == 0) {
-        //  return Text('uitverkocht');
-          return Container();
-        } else {
-          return ListTile(
-            title: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: ListTile(
-                          isThreeLine: true,
-                          leading: CircleAvatar(
-                            backgroundImage:
-                            CachedNetworkImageProvider(_listing.beer.label.iconUrl,),
-                          ),
-                          title: Text(_listing.beer.name,
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(_listing.beer.brewery),
-                              Text(_listing.beer.abv.toString() + "%"),
-                              Text(_listing.beer.style.name),
-                              //   Text( _listing.beer.rating.toString()),
-                            ],
-                          ),
-                          ),
-                    ),
-                    Expanded(
-                      child: Column(
+class _FirestoreStockListviewState extends State<FirestoreStockListview> {
+  final searchController = TextEditingController();
+  String filterString = "";
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is removed from the Widget tree
+    searchController.dispose();
+    super.dispose();
+  }
+  @override
+  void initState() {
+    super.initState();
+
+    // Start listening to changes
+    searchController.addListener(_updateSearchItems);
+  }
+
+  _updateSearchItems(){
+    this.setState((){
+      filterString = searchController.text.toLowerCase();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8.0,0,0,0),
+          child: TextFormField(
+            controller: searchController,
+              decoration: const InputDecoration(
+              icon: Icon(Icons.search),
+              hintText: 'Filter op naam of style of brouwerij of beschrijving: "Sweet" "Smoke" "Vanilla" ',
+                labelText: 'Zoeken',),
+         /*   onFieldSubmitted: (String sString) {
+                this.setState((){
+                  searchString = sString.toLowerCase();
+                  print(searchString);
+                });
+            },*/
+
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: widget.beersInStock.length,
+            itemBuilder: (BuildContext context, int index) {
+              var _listing = BeerListing(
+                beer: Beer(
+                  id: widget.beersInStock[index].data['id'].toString(),
+                  name: widget.beersInStock[index].data['name'].toString(),
+                  brewery: widget.beersInStock[index].data['brewery'].toString(),
+                  label: BeerLabel(
+                      iconUrl: widget.beersInStock[index].data['label']['iconUrl'].toString()),
+                  style: BeerStyle(
+                      id: null, name: widget.beersInStock[index].data['style'].toString()),
+                  abv: widget.beersInStock[index].data['abv'],
+                  rating: widget.beersInStock[index].data['rating'],
+                  description: widget.beersInStock[index].data['desc'],
+                  tasteDescription: widget.beersInStock[index].data['tasteDesc']
+                ),
+                price: widget.beersInStock[index].data['price'],
+                stockAmount: widget.beersInStock[index].data['amount'],
+              );
+
+              if (_listing.stockAmount == 0) {
+              //  return Text('uitverkocht');
+                return Container();
+              } else if (filterString != '')
+                {
+                  if (_listing.beer.name.toLowerCase().contains(filterString) | _listing.beer.style.name.toLowerCase().contains(filterString) | _listing.beer.brewery.toLowerCase().contains(filterString) | _listing.beer.description.toLowerCase().contains(filterString) | _listing.beer.tasteDescription.toLowerCase().contains(filterString)){
+                return ListTile(
+                  title: Column(
+                    children: <Widget>[
+                      Row(
                         children: <Widget>[
-                          StarRating(
-                            rating: toRound(_listing.beer.rating),
-                            color: Colors.white,
-                            borderColor: Colors.white,
+                          Expanded(
+                            child: ListTile(
+                                isThreeLine: true,
+                                leading: CircleAvatar(
+                                  backgroundImage:
+                                  CachedNetworkImageProvider(_listing.beer.label.iconUrl,),
+                                ),
+                                title: Text(_listing.beer.name,
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(_listing.beer.brewery),
+                                    Text(_listing.beer.abv.toString() + "%"),
+                                    Text(_listing.beer.style.name),
+                                    //   Text( _listing.beer.rating.toString()),
+                                  ],
+                                ),
+                                ),
                           ),
-                          Text(_listing.beer.rating.toString()),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                StarRating(
+                                  rating: toRound(_listing.beer.rating),
+                                  color: Colors.white,
+                                  borderColor: Colors.white,
+                                ),
+                                Text(_listing.beer.rating.toString()),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                              child: quickaddButton(
+                                  _listing.stockAmount, _listing.price))
                         ],
                       ),
-                    ),
-                    Expanded(
-                        child: quickaddButton(
-                            _listing.stockAmount, _listing.price))
-                  ],
-                ),
-                Divider(
-                  height: 15.0,
-                  color: Colors.white,
-                ),
-              ],
-            ),
-            onTap:() {
-              Navigator.push(context, new MaterialPageRoute(builder: (context) => BeerDetailPage(_listing.beer.id)));
+                      Divider(
+                        height: 15.0,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  onTap:() {
+                    Navigator.push(context, new MaterialPageRoute(builder: (context) => BeerDetailPage(_listing.beer.id)));
+                  },
+                );
+                  }
+                  else {
+                    return Container();
+                  }
+                }
+              else if(filterString == '') {
+                return ListTile(
+                  title: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: ListTile(
+                              isThreeLine: true,
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                CachedNetworkImageProvider(_listing.beer.label.iconUrl,),
+                              ),
+                              title: Text(_listing.beer.name,
+                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(_listing.beer.brewery),
+                                  Text(_listing.beer.abv.toString() + "%"),
+                                  Text(_listing.beer.style.name),
+                                  //   Text( _listing.beer.rating.toString()),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                StarRating(
+                                  rating: toRound(_listing.beer.rating),
+                                  color: Colors.white,
+                                  borderColor: Colors.white,
+                                ),
+                                Text(_listing.beer.rating.toString()),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                              child: quickaddButton(
+                                  _listing.stockAmount, _listing.price))
+                        ],
+                      ),
+                      Divider(
+                        height: 15.0,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  onTap:() {
+                    Navigator.push(context, new MaterialPageRoute(builder: (context) => BeerDetailPage(_listing.beer.id)));
+                  },
+                );
+              }
             },
-          );
-        }
-      },
+          ),
+        ),
+      ],
     );
   }
 }
